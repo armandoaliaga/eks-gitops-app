f76f3712a8bfbcaf38a82da7cf5217fd783d28f7
Armando Aliaga <armando.aliaga@ascbomacbook12.local>
2026-05-22 16:41:42 -0400

    feat: update message to v2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello from EKS!',
+    message: 'Hello from EKS! - v2',
     version: process.env.APP_VERSION || 'v1',
     pod: process.env.HOSTNAME,
   });
